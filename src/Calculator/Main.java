5cc2385de525bb70b51386b17a13e3e3a2337074
Marko Cvijetic <cvijetic.ra40.2019@uns.ac.rs>
2021-10-23 18:52:00 +0200

    initial read loop

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -1,10 +1,16 @@
 package Calculator;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner;
+		scanner=new Scanner(System.in);
+		String s;
+		do {
+			 s=scanner.nextLine(); 	
+		 }while(!s.equals("kraj"));
 	}
-
 }
