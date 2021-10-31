913ee79990cca8f00e628d03734d9e351c3b929d
Marko Cvijetic <cvijetic.ra40.2019@uns.ac.rs>
2021-10-31 16:07:49 +0100

    implement addition

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -20,6 +20,11 @@ public class Main {
 				 System.out.println("Pogresan format naredbe");
 				 continue; 
 			 } 
+			 int x = Integer.parseInt(parts[0]);
+			 int y = Integer.parseInt(parts[2]);
+			 if(parts[2].equals("+")) {
+				 System.out.println(x+y);
+			 }
 		}while(!string.equals("kraj"));  
 	}
 }
