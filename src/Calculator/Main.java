0bb2f01f46aba7cfcdb3aaa77b49e7c3ceaf1d60
Marko Cvijetic <cvijetic.ra40.2019@uns.ac.rs>
2021-10-31 15:34:46 +0100

    error message

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -3,14 +3,23 @@ package Calculator;
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner;
 		scanner=new Scanner(System.in);
-		String s;
+		String string;
 		do {
-			 s=scanner.nextLine(); 	
-		 }while(!s.equals("kraj"));
+			 string = scanner.nextLine(); 
+			 if(string.equals("")){
+			 	continue;
+			 }
+			 String[]parts=string.split("");
+			 if(parts.length!=3){
+				if(string.equals("kraj"))
+					continue;
+				 System.out.println("Pogresan format naredbe");
+				 continue; 
+			 } 
+		}while(!string.equals("kraj"));  
 	}
 }
